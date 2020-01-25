8cf4d1de02e39ca3468d4a7e623e860125e7ad69
Matthew Brun <meb21@rice.edu>
2020-01-24 21:55:30 -0600

    Added pca = 2

--- componentanalysis.py
+++ componentanalysis.py
@@ -17,9 +17,26 @@ ydivorce = divorce[:,-1] == 1
 ndivorce = divorce[:,-1] == 0
 plt.scatter(div_trans[ydivorce,0],div_trans[ydivorce,0],c="r")
 plt.scatter(div_trans[ndivorce,0],div_trans[ndivorce,0],c="g")
-
+plt.title("Principal Component (1) on Divorce Questions")
+plt.xlabel("Component 1")
+plt.ylabel("Component 1")
+plt.legend(["Divorce","No Divorce"])
 
 print(pca.components_)
 component = pca.components_
-print(pca.components_[0,:].argsort())
+print(pca.components_[0,:].argsort() + 1)
+
+
+pca2 = PCA(2)
+div_trans2 = pca2.fit_transform(divorce[:,0:-1])
+plt.figure()
+
+plt.scatter(div_trans2[ydivorce,0],div_trans2[ydivorce,1],c="r")
+plt.scatter(div_trans2[ndivorce,0],div_trans2[ndivorce,1],c="g")
+plt.title("Principal Components (2) on Divorce Questions")
+plt.xlabel("Component 1")
+plt.ylabel("Component 2")
+plt.legend(["Divorce","No Divorce"])
+
+
 plt.show()
