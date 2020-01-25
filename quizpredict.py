772a80b45e0e1cc45e5b8e3ef4820a76f81ca161
Matthew Brun <meb21@rice.edu>
2020-01-25 13:31:10 -0600

    Console survey added

--- quizpredict.py
+++ quizpredict.py
@@ -7,6 +7,7 @@ from sklearn.decomposition import PCA
 from scipy.stats import pearsonr
 import statistics
 import pickle
+import sys
 
 #!/usr/bin/env python
 
@@ -18,3 +19,27 @@ def predict(q35, q36, q40):
     result = clf.predict(response)
 
     return(result)
+
+
+print("On a scale of 1 to 5, to what degree do you agree with the following statements?\n")
+print("I can insult my spouse during our discussions")
+q35 = float(input("Response: "))
+while q35 not in [1, 2, 3, 4, 5]:
+    q35 = float(input("Please enter an integer between 1 and 5: "))
+
+print("I can be humiliating during discussions with my spouse")
+q36 = float(input("Response: "))
+while q36 not in [1, 2, 3, 4, 5]:
+    q36 = float(input("Please enter an integer between 1 and 5: "))
+
+print("I am just starting a discussion with my spouse before I know what's going on")
+q40 = float(input("Response: "))
+while q40 not in [1, 2, 3, 4, 5]:
+    q40 = float(input("Please enter an integer between 1 and 5: "))
+
+result = predict(q35-1, q36-1, q40-1)
+
+if result == 1:
+    print("You will get divorced!")
+else:
+    print("You will not get divorced!")
