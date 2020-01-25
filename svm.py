e8cc2ecc11fae1a0db88c831754b147a4c14ba87
Matthew Brun <meb21@rice.edu>
2020-01-24 22:00:53 -0600

    Added Datasets/svm file

---
+++ svm.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas
+import sklearn
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
+
+import os
+from matplotlib import pyplot as plt
+
+os.chdir("/Users/matthewbrun/Spring 2020/Datathon/")
+
+
+divorce = np.loadtxt("divorce.csv", delimiter=";", skiprows=1)
+
+
